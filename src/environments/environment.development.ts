b7eb76b3318399e036981cba1267ef918232b309
Victor Vaz <triceps_mettle_0f@icloud.com>
2023-09-09 20:04:38 -0300

    Setting Firebase hosting.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyBKcHyyu9zoL9hHnoRPia-dhaovgJJv5V0",
+        authDomain: "victor-vaz.firebaseapp.com",
+        projectId: "victor-vaz",
+        storageBucket: "victor-vaz.appspot.com",
+        messagingSenderId: "92756499609",
+        appId: "1:92756499609:web:4bfd39b08b3732b66b6c08",
+        measurementId: "G-2MYR00HKD9"
+      }
+};
